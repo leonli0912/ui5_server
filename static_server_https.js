751ae7f02e0c033b4a9c4a02ad1fcfd100d10409
leonli0912 <ly95487532@163.com>
2019-01-04 16:13:02 +0800

    post request with cookie

--- static_server_https.js
+++ static_server_https.js
@@ -1,23 +1,66 @@
 const express = require('express')
 const app = express()
-const appconfig = require('fioriSandboxConfig');
-var proxy = require('express-http-proxy');
-
-var httpProxy = require('http-proxy');
-
-var odataProxy = httpProxy.createProxyServer({ target: 'https://ldai4er9.wdf.sap.corp:44300', secure: false });
-odataProxy.on('proxyReq', function (proxyReq, req, res, options) {
-  //console.log('enter proxy..');
-  proxyReq.setHeader('sap-client', '500');
+const proxy = require('express-http-proxy');
+const httpProxy = require('http-proxy');
+const proxyTargets = {
+	er9: {
+		target: 'https://ldai4er9.wdf.sap.corp:44300',
+		client: '500'
+	},
+	ccf: {
+		target: 'https://ldciccf.wdf.sap.corp:44300',
+		client: '715'
+	}
+}
+const odataProxy = httpProxy.createProxyServer({
+	target: proxyTargets.ccf.target,
+	secure: false
 });
 
-app.route('/sap/opu/odata/*$').all(function (req, res) {
-  odataProxy.web(req, res);
+var COOKIE = null;
+
+odataProxy.on('proxyReq', function(proxyReq, req, res, options) {
+	if (req.method === 'HEAD') {
+		console.log('request url....:', req.url);
+		console.log('request headers....:', req.headers.cookie);
+		var cookies = req.headers.cookie.split(";");
+		for (var i = cookies.length - 1; i >= 0; i--) {
+			if(cookies[i].includes("SAP_SESSIONID_CCF_715")){
+				COOKIE = cookies[i];
+			}
+		}
+		//proxyReq.setHeader('x-csrf-token', 'fetch');
+
+	}
+	proxyReq.setHeader('sap-client', proxyTargets.ccf.client);
+	if (req.method === 'POST') {
+		console.log('CSRF:...', CSRF);
+		if (CSRF) {
+			//proxyReq.setHeader('Content-Type', 'Application/atom+xml');
+			//proxyReq.setHeader('x-csrf-token', CSRF);
+			proxyReq.setHeader('X-Requested-With','XMLHttpRequest');
+			proxyReq.setHeader('cookie',COOKIE);
+		}
+
+	}
+});
+
+odataProxy.on('proxyRes', function(proxyRes, req, res) {
+	if (req.method === 'HEAD') {
+		console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2));
+		CSRF = proxyRes.headers["x-csrf-token"];
+		console.log(CSRF);
+	}
+
+});
+
+app.route('/sap/opu/odata/*$').all(function(req, res) {
+	odataProxy.web(req, res);
 });
 
 app.use(express.static(__dirname));
 app.use('/resources/', proxy('sapui5.hana.ondemand.com/resources/', {
-  https: true
+	https: true
 }));
 /* app.use('/test-resources/', proxy('sapui5.hana.ondemand.com/sdk/test-resources/', {
   https: true
